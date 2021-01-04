9d1f06f804e6d56ded5857edb0b2633ea22b6b55
m0sirhy <m0sirhy@gmail.com>
2021-01-04 11:36:56 +0200

    add S-Employee Mangment

---
+++ database/migrations/2021_01_04_092654_create_salaries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->double('salary_amount');
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->string('receipt_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
